0c8f81b4dc00dcdc2248b4a1a0b9b52e2cd700af
MalikNeelam06 <128276286+MalikNeelam06@users.noreply.github.com>
2023-07-23 15:31:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function threeSum(arr, target) {
 // write your code here
+	nums.sort((a, b) => a - b); 
+	let mainSum = Infinity; 
+	for (let i = 0; i < nums.length - 2; i++)
+		{ let a = i + 1; let b = nums.length - 1;
+		 while (a < b)
+			 { const CS = nums[i] + nums[a] + nums[b]; 
+	if (Math.abs(target - CS) < Math.abs(target - mainSum)) 
+	{ mainSum = CS; } 
+			  if (CS < target) { a++; }
+			  else if (CS > target) { b--; }
+	else { return mainSum; } } } 
+	return mainSum;
   
 }
 
